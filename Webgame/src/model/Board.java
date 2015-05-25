32dca5f9f8125842814f2aa37c4ac4b43db38cf8
sx813 <sx813@ic.ac.uk>
2015-05-25 15:57:01 +0100

    Add some functions in Player and Board class

--- Webgame/src/model/Board.java
+++ Webgame/src/model/Board.java
@@ -15,18 +15,55 @@ public class Board {
 	private List<Obstacle> obstacles = new ArrayList<Obstacle>();
 	private List<Monster> monsters = new ArrayList<Monster>();
 	private Player player;
+	private int numOfMonsters;
+	private int numOfObstacles;
 	
-	public Board(Player player) {
+	public Board(Player player, int level) {
 		this.player = player;
 		board = new Object[HEIGHT][WIDTH];
+		numOfMonsters = level * level;
+		numOfObstacles = level * 7;
 		for (int i = 0; i < WIDTH; i++) {
 			for (int j = 0; j < HEIGHT; j++) {
 				board[j][i] = null;
 			}
 		}
+		
+		//add number of monsters related to player's level 
+		for (int i = 0; i < numOfMonsters; i++) {
+			int x = random.nextInt(WIDTH);
+			int y = random.nextInt(HEIGHT);
+			while (board[y][x] != null) {
+				x = random.nextInt(WIDTH);
+				y = random.nextInt(HEIGHT);
+			}
+			board[y][x] = new Monster(x, y);
+			monsters.add((Monster) board[y][x]);
+		}
+		//add number of obstacles related to player's level
+		for (int i = 0; i < numOfObstacles; i++) {
+			int x = random.nextInt(WIDTH);
+			int y = random.nextInt(HEIGHT);
+			while (board[y][x] != null) {
+				x = random.nextInt(WIDTH);
+				y = random.nextInt(HEIGHT);
+			}
+			board[y][x] = new Obstacle(x, y);
+			obstacles.add((Obstacle) board[y][x]);
+		}
 	}
 	
-	//generate enemy and obstacle
+	public List<Obstacle> getObstacleList() {
+		return obstacles;
+	}
+	
+	public List<Monster> getMonsters() {
+		return monsters;
+	}
+	
+	public Player getPlayer() {
+		return player;
+	}
 
 }
 
--- Webgame/src/model/Player.java
+++ Webgame/src/model/Player.java
@@ -1,5 +1,36 @@
 package model;
 
 public class Player {
+	
+	private int x;
+	private int y;
+	private int level;
+	private Direction dir;
+	
+	public Player(int level) {
+		this.level = level;
+		setX(14);
+		setY(14);
+		dir = Direction.NORTH;
+	}
 
+//	public Direction rotate() {
+//		dir = (dir + 1) % 4;
+//	}
+//	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
 }
