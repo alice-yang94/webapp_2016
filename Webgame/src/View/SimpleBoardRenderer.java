2cbc4bd9389b23e13c7483e3ce4c4a8e4ac3fbf0
Wenke Yang <wenke.yang13@imperial.ac.uk>
2015-05-31 21:07:42 +0100

    add render seeds

--- Webgame/src/View/SimpleBoardRenderer.java
+++ Webgame/src/View/SimpleBoardRenderer.java
@@ -6,43 +6,54 @@ import java.awt.Graphics;
 import model.Board;
 import model.Monster;
 import model.Player;
+import model.Seed;
 
 public class SimpleBoardRenderer implements Renderer {
 
 	private Board board;
-	
+
 	public SimpleBoardRenderer(Board board) {
 		this.board = board;
 	}
-	
+
 	@Override
 	public void render(Graphics g) {
-		//render the board
+		// render the board
 		int cellSize = 25;
 		g.setColor(Color.blue);
-		for (int i = 0; i< Board.WIDTH; i++) {
+		for (int i = 0; i < Board.WIDTH; i++) {
 			g.drawLine(i * cellSize, 0, i * cellSize, Board.HEIGHT * cellSize);
 			if (i < Board.WIDTH) {
-				g.drawLine(0, i * cellSize, Board.WIDTH * cellSize, i * cellSize);
+				g.drawLine(0, i * cellSize, Board.WIDTH * cellSize, i
+						* cellSize);
 			}
 		}
-		
-		//render player
+
+		// render player
 		g.setColor(Color.red);
 		Player player = board.getPlayer();
 		int x = (int) (player.getX() * cellSize);
 		int y = (int) (player.getY() * cellSize);
 		g.fillOval(x + 2, y + 2, cellSize - 4, cellSize - 4);
-		//render square on player
+
+		
+		// render square on player
 		g.setColor(Color.white);
 		g.fillRect(x + 10, y + 10, cellSize - 20, cellSize - 20);
-		
-		
-		//render monsters
+
+		// render monsters
 		g.setColor(Color.green);
 		for (Monster monster : board.getMonsters()) {
 			x = (int) (monster.getX() * cellSize);
 			y = (int) (monster.getY() * cellSize);
+			g.fillOval(x + 2, y + 2, cellSize - 4, cellSize - 4);
+		}
+
+		// render seeds
+		g.setColor(Color.yellow);
+		for (Seed seed : board.getSeeds()) {
+			x = (int) (seed.getX() * cellSize);
+			y = (int) (seed.getY() * cellSize);
 			g.fillOval(x + 2, y + 2, cellSize - 4, cellSize - 4);
 		}
 	}
--- Webgame/src/model/Board.java
+++ Webgame/src/model/Board.java
@@ -8,16 +8,16 @@ public class Board {
 
 	public static final int WIDTH = 750 / 25;
 	public static final int HEIGHT = 750 / 25;
-	
+
 	private static Random random = new Random();
-	
+
 	private Object[][] board;
 	private List<Monster> monsters = new ArrayList<Monster>();
 	private List<Seed> seeds = new ArrayList<Seed>();
 	private Player player;
 	private int numOfMonsters;
 	private int numOfSeeds;
-	
+
 	public Board(Player player) {
 		this.player = player;
 		int level = player.getLevel();
@@ -29,13 +29,13 @@ public class Board {
 				board[j][i] = null;
 			}
 		}
-		
-		//add player to the center of board
+
+		// add player to the center of board
 		board[(int) player.getY()][player.getX()] = player;
-		
-		//add number of monsters with seeds related to player's level 
+
+		// add number of monsters with seeds related to player's level
 		for (int i = 0; i < numOfMonsters; i++) {
-			//generate monsters
+			// generate monsters
 			int x = random.nextInt(WIDTH);
 			int y = random.nextInt(HEIGHT);
 			while (board[y][x] != null) {
@@ -44,41 +44,47 @@ public class Board {
 			}
 			board[y][x] = new Monster(x, y);
 			monsters.add((Monster) board[y][x]);
-			
-			//generate seeds
+
+			// generate seeds
 			if (random.nextInt(2) == 1) {
-				int sx = generateNeighbourPoint(x);
-				int sy = generateNeighbourPoint(y);
+				int sx = generateNeighbourPoint(x, true);
+				int sy = generateNeighbourPoint(y, false);
 				while (board[sy][sx] != null) {
-					sx = generateNeighbourPoint(x);
-					sy = generateNeighbourPoint(y);
+					sx = generateNeighbourPoint(x, true);
+					sy = generateNeighbourPoint(y, false);
 				}
 				board[sy][sx] = new Seed(sx, sy);
 				seeds.add((Seed) board[sy][sx]);
-			}	
+			}
 		}
 	}
 
-	private int generateNeighbourPoint(int x) {     //assume width and height are the same
-		int result = x - 2 + random.nextInt(4);
-		while (result < 0 || result >= WIDTH) {
+	private int generateNeighbourPoint(int x, boolean isWidth) { 
+		int result = x - 1 + random.nextInt(3);
+		int compareWith;
+		if (isWidth) {
+			compareWith = WIDTH;
+		} else {
+			compareWith = HEIGHT;
+		}
+		while (result < 0 || result >= compareWith) {
 			result = x - 2 + random.nextInt();
 		}
 		return result;
 	}
-	
+
 	public List<Monster> getMonsters() {
 		return monsters;
 	}
-	
+
 	public List<Seed> getSeeds() {
 		return seeds;
 	}
-	
+
 	public Player getPlayer() {
 		return player;
 	}
-	
+
 	public void removeDeadMonster(Monster deadMonster) throws Exception {
 		if (!monsters.remove(deadMonster)) {
 			throw new Exception("The Game has Bugs!");
@@ -89,9 +95,9 @@ public class Board {
 	public int getNumOfSeeds() {
 		return numOfSeeds;
 	}
-	
-//	public Object[][]  getBoard() {
-//		return board;
-//	}
+
+	// public Object[][] getBoard() {
+	// return board;
+	// }
 
 }
