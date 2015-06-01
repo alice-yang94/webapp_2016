4de140fd0c13a6699e7a48b9246d33a8c25c1d50
Adam Davies <ad5013@edge21.doc.ic.ac.uk>
2015-06-01 11:33:06 +0100

    Implemented salt in encryption. Now returns a PasswordPair that has the encrypted password and salt value.

---
+++ login/PasswordPair.java
@@ -0,0 +1,21 @@
+package encryption;
+
+public class PasswordPair {
+  
+  private String password;
+  private String salt;
+
+  public PasswordPair(String password, String salt) {
+    this.password = password;
+    this.salt = salt;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getSalt() {
+    return salt;
+  }
+
+}
