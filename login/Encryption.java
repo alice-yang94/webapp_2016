4490d49e2a1e0bd5abdc0b418689d3c21d979164
shanning <shanning1@gmail.com>
2015-06-09 17:41:59 +0100

    updates to website and servlets

--- login/Encryption.java
+++ login/Encryption.java
@@ -1,5 +1,3 @@
-package login;
-
 /* Encryption imports */
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
--- main/MainServlet.java
+++ main/MainServlet.java
@@ -17,30 +17,81 @@ public class MainServlet extends HttpServlet {
 
     String res = "";
 
-    try {
-      Connection conn = DriverManager.getConnection(dbConnString, dbUsername, dbPassword);
-      
-      Statement statement = conn.createStatement();
-      
-      ResultSet rs = statement.executeQuery("SELECT * FROM completedGames ORDER BY score DESC");
-      int i = 1;
-      while (rs.next() && i < 10) {
-        res += "<div class=\"well well-sm\"><b>" + i + ": " + rs.getString("username") 
-                + "</b><br />" + rs.getString("score") + "</div>";
-        i++;
+    String action = request.getParameter("action");
+
+    if ("userGames".equals(action)) {
+      Cookie ck[] = request.getCookies();
+      String username = "";
+      if (ck != null) username = ck[0].getValue();
+
+
+      res += "<h1>" + username + "'s recent games: </h1><br />";
+
+      try {
+        Connection conn = DriverManager.getConnection(dbConnString, dbUsername, dbPassword);
+        
+        Statement statement = conn.createStatement();
+        
+        ResultSet rs = statement.executeQuery("SELECT * FROM completedGames WHERE username='" + 
+                            username + "' ORDER BY dateCompleted DESC");
+
+        int i = 1;
+        while (rs.next() && i < 10) {
+          if (i % 3 == 1) res += "<div class=\"col-md-4\">";
+
+          res += "<div class=\"well well-sm\"><b>" + rs.getString("dateCompleted") + 
+                  "</b><br />" + rs.getString("score") + "</div>";
+
+          if (i % 3 == 0) res += "</div>";
+          i++;
+        }
+      } catch (Exception e) {
+        for (int x = 0; x < 3; x++) {
+          res += "<div class=\"row\">";
+
+          for (int y = 0; y < 3; y++) {
+            res += "<div class=\"col-md-4\"><div class=\"well well-sm\"><b>1/1/1970</b><br />999999</div></div>";
+          }
+
+          res += "</div>";
+        }
       }
 
-      conn.close();
-    } catch (Exception e) { //print dummy data as database could not be accessed
-      for (int x = 1; x < 10; x++) {
-        res += "<div class=\"well well-sm\"><b>" + x + ": player</b><br />999999</div>";
+      response.setContentType("text/html");
+
+      PrintWriter out = response.getWriter();
+
+      out.println(res);
+      return;
+
+    } else if ("highScores".equals(action)) {
+      try {
+        Connection conn = DriverManager.getConnection(dbConnString, dbUsername, dbPassword);
+        
+        Statement statement = conn.createStatement();
+        
+        ResultSet rs = statement.executeQuery("SELECT * FROM completedGames ORDER BY score DESC");
+        int i = 1;
+        while (rs.next() && i < 10) {
+          res += "<div class=\"well well-sm\"><b>" + i + ": " + rs.getString("username") +
+                  " - " + rs.getString("dateCompleted") + "</b><br />" + rs.getString("score") + "</div>";
+          i++;
+        }
+
+        conn.close();
+      } catch (Exception e) { //print dummy data as database could not be accessed
+        for (int x = 1; x < 10; x++) {
+          res += "<div class=\"well well-sm\"><b>" + x + ": player - 1/1/1970</b><br />999999</div>";
+        }
       }
+
+      response.setContentType("text/html");
+
+      PrintWriter out = response.getWriter();
+
+      out.println(res);
+      return;
     }
 
-    response.setContentType("text/html");
-
-    PrintWriter out = response.getWriter();
-
-    out.println(res);
   }
 }
